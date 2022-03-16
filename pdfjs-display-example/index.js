d168d4f73798b3bad5fd2f5ec3d1b06df8952a04
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2022-03-16 12:50:38 -0700

    update example

--- pdfjs-display-example/index.js
+++ pdfjs-display-example/index.js
@@ -1,24 +1,113 @@
-(async () => {
-  const loadingTask = pdfjsLib.getDocument("document.pdf");
-  const pdf = await loadingTask.promise;
+const pdf = "document.pdf";
 
-  // Load information from the first page.
-  const page = await pdf.getPage(1);
+const pageNum = document.querySelector("#page_num");
+const pageCount = document.querySelector("#page_count");
+const currentPage = document.querySelector("#current_page");
+const previousPage = document.querySelector("#prev_page");
+const nextPage = document.querySelector("#next_page");
+const zoomIn = document.querySelector("#zoom_in");
+const zoomOut = document.querySelector("#zoom_out");
 
-  const scale = 1;
-  const viewport = page.getViewport({ scale });
+const initialState = {
+  pdfDoc: null,
+  currentPage: 1,
+  pageCount: 0,
+  zoom: 1,
+};
 
-  // Apply page dimensions to the `<canvas>` element.
-  const canvas = document.getElementById("pdf");
-  const context = canvas.getContext("2d");
-  canvas.height = viewport.height;
-  canvas.width = viewport.width;
+// Render the page
+const renderPage = () => {
+  // load the first page
+  console.log(initialState.pdfDoc, "pdfDoc");
+  initialState.pdfDoc.getPage(initialState.currentPage).then((page) => {
+    console.log("page", page);
 
-  // Render the page into the `<canvas>` element.
-  const renderContext = {
-    canvasContext: context,
-    viewport: viewport,
-  };
-  await page.render(renderContext);
-  console.log("Page rendered!");
-})();
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+    const viewport = page.getViewport({ scale: initialState.zoom });
+
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Render PDF page into canvas context
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport: viewport,
+    };
+
+    page.render(renderCtx);
+
+    pageNum.textContent = initialState.currentPage;
+  });
+};
+
+// Load the Document
+pdfjsLib
+  .getDocument(pdf)
+  .promise.then((data) => {
+    initialState.pdfDoc = data;
+    console.log("pdfDocument", initialState.pdfDoc);
+
+    pageCount.textContent = initialState.pdfDoc.numPages;
+
+    renderPage();
+  })
+  .catch((err) => {
+    alert(err.message);
+  });
+
+const showPrevPage = () => {
+  if (initialState.pdfDoc === null || initialState.currentPage <= 1) return;
+  initialState.currentPage--;
+  // render the current page
+  currentPage.value = initialState.currentPage;
+  renderPage();
+};
+
+const showNextPage = () => {
+  if (
+    initialState.pdfDoc === null ||
+    initialState.currentPage >= initialState.pdfDoc._pdfInfo.numPages
+  )
+    return;
+
+  initialState.currentPage++;
+  currentPage.value = initialState.currentPage;
+  renderPage();
+};
+
+// Button Events
+previousPage.addEventListener("click", showPrevPage);
+nextPage.addEventListener("click", showNextPage);
+
+// Keypress Event
+currentPage.addEventListener("keypress", (event) => {
+  if (initialState.pdfDoc === null) return;
+  // get the key code
+  const keycode = event.keyCode ? event.keyCode : event.which;
+
+  if (keycode === 13) {
+    // get the new page number and render it
+    let desiredPage = currentPage.valueAsNumber;
+    initialState.currentPage = Math.min(
+      Math.max(desiredPage, 1),
+      initialState.pdfDoc._pdfInfo.numPages
+    );
+
+    currentPage.value = initialState.currentPage;
+    renderPage();
+  }
+});
+
+// Zoom Events
+zoomIn.addEventListener("click", () => {
+  if (initialState.pdfDoc === null) return;
+  initialState.zoom *= 4 / 3;
+  renderPage();
+});
+
+zoomOut.addEventListener("click", () => {
+  if (initialState.pdfDoc === null) return;
+  initialState.zoom *= 2 / 3;
+  renderPage();
+});
